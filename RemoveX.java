42b48025c9daedba6df828a822cb97432da5de51
Ujjwal kumar <102848863+kUjjwal69@users.noreply.github.com>
2023-04-19 11:46:31 +0530

    Create RemoveX.java

---
+++ RemoveX.java
@@ -0,0 +1,48 @@
+/*
+Given a string, compute recursively a new string where all 'x' chars have been removed.
+Input format :
+String S
+Output format :
+Modified String
+Constraints :
+1 <= |S| <= 10^3
+where |S| represents the length of string S. 
+Sample Input 1 :
+xaxb
+Sample Output 1:
+ab
+Sample Input 2 :
+abc
+Sample Output 2:
+abc
+Sample Input 3 :
+xx
+Sample Output 3:
+*/
+
+
+public class solution {
+
+	// Return the changed string
+	public static String removeX(String input){
+
+    return remove_X( input);
+
+	}
+
+	public static String remove_X(String input){
+
+		 if(input.length()==0){
+
+		 return "";
+
+	 }
+	 if(input.charAt(0)=='x'){
+		 return remove_X(input.substring(1));
+	 }
+	 	return input.charAt(0) +
+				remove_X(input.substring(1));
+
+	}
+
+}
